194414ea90520dced8ffd49499fb2fef6c6be9d5
Maarten Grachten <maarten.grachten@gmail.com>
2024-04-06 00:28:56 +0200

    use FAD in example 2

--- examples/example_2.py
+++ examples/example_2.py
@@ -28,7 +28,7 @@ real1_items = multi_audio_slicer(real1_items, win_dur)
 fake1_items = multi_audio_slicer(fake1_items, win_dur)
 real2_items = multi_audio_slicer(real2_items, win_dur)
 
-metrics = AudioPromptAdherence(dev, win_dur, n_pca=100, embedder="clap", metric="mmd")
+metrics = AudioPromptAdherence(dev, win_dur, n_pca=100, embedder="clap", metric="fad")
 metrics.set_background(real1_items)
 print("non-matching:")
 result = metrics.compare_to_background(fake1_items)
