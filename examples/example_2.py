be48bd1ae16be8892f6396988f0f72b6d3e1438c
Maarten Grachten <maarten.grachten@gmail.com>
2024-09-26 12:59:00 +0200

    refactor example 2

--- examples/example_2.py
+++ examples/example_2.py
@@ -9,32 +9,51 @@ from audio_metrics import (
 from audio_metrics.example_utils import generate_audio_samples
 
 
-audio_dir1 = Path("audio_samples1")
-audio_dir2 = Path("audio_samples2")
+def get_data_iterators(win_dur):
+    audio_dir1 = Path("audio_samples1")
+    audio_dir2 = Path("audio_samples2")
+
+    print("generating 'real' and 'fake' audio samples")
+    generate_audio_samples(audio_dir1)
+    generate_audio_samples(audio_dir2)
+
+    # load audio samples from files in `audio_dir`
+    real1_song_iterator = async_audio_loader(audio_dir1 / "real", mono=False)
+    fake1_song_iterator = async_audio_loader(audio_dir1 / "fake", mono=False)
+    real2_song_iterator = async_audio_loader(audio_dir2 / "real", mono=False)
+
+    # iterate over windows
+    real1_win_iterator = multi_audio_slicer(real1_song_iterator, win_dur)
+    fake1_win_iterator = multi_audio_slicer(fake1_song_iterator, win_dur)
+    real2_win_iterator = multi_audio_slicer(real2_song_iterator, win_dur)
+
+    return (
+        real1_win_iterator,
+        fake1_win_iterator,
+        real2_win_iterator,
+    )
+
+
 win_dur = 5.0
-dev = torch.device("cuda")
+(
+    real1_win_iterator,
+    fake1_win_iterator,
+    real2_win_iterator,
+) = get_data_iterators(win_dur)
 
-print("generating 'real' and 'fake' audio samples")
-generate_audio_samples(audio_dir1)
-generate_audio_samples(audio_dir2)
-
-# load audio samples from files in `audio_dir`
-real1_items = async_audio_loader(audio_dir1 / "real", mono=False)
-fake1_items = async_audio_loader(audio_dir1 / "fake", mono=False)
-real2_items = async_audio_loader(audio_dir2 / "real", mono=False)
-
-# iterate over windows
-real1_items = multi_audio_slicer(real1_items, win_dur)
-fake1_items = multi_audio_slicer(fake1_items, win_dur)
-real2_items = multi_audio_slicer(real2_items, win_dur)
-
+dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 metrics = AudioPromptAdherence(
-    dev, win_dur, n_pca=100, embedder="clap", metric="fad", layer="audio_projection.2"
+    dev,
+    win_dur,
+    n_pca=None,
+    embedder="clap_music",
+    metric="fad",
+    layer="audio_projection.2",
 )
-metrics.set_background(real1_items)
+metrics.set_background(real1_win_iterator)
 print("non-matching:")
-result = metrics.compare_to_background(fake1_items)
+result = metrics.compare_to_background(fake1_win_iterator)
 print(json.dumps(result, indent=2))
 print("matching:")
-result = metrics.compare_to_background(real2_items)
+result = metrics.compare_to_background(real2_win_iterator)
 print(json.dumps(result, indent=2))
