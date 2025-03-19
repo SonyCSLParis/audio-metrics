55a0232b03d915821a5ad3b13be5fe0acf9970b6
Maarten Grachten <maarten@grachten.eu>
2025-03-19 11:18:28 +0100

    move to uv-compatible pyproject.toml; leaner import; disable openl3; fix import;

--- src/audio_metrics/__init__.py
+++ src/audio_metrics/__init__.py
@@ -1,14 +1,14 @@
 import enum
-from .clap import CLAP
-from .vggish import VGGish
+# from .clap import CLAP
+# from .vggish import VGGish
 
-try:
-    import torchopenl3
-except ModuleNotFoundError:
-    have_openl3 = False
-else:
-    have_openl3 = True
-    from .openl3 import OpenL3
+# try:
+#     import torchopenl3
+# except ModuleNotFoundError:
+#     have_openl3 = False
+# else:
+#     have_openl3 = True
+#     from .openl3 import OpenL3
 
 
 Embedder = enum.Enum(
@@ -21,11 +21,12 @@ Embedder = enum.Enum(
             "clap_music",
             "clap_music_speech",
         ]
-        + (["openl3"] if have_openl3 else [])
+        # + (["openl3"] if have_openl3 else [])
     },
 )
 
 from .dataset import async_audio_loader, multi_audio_slicer
 from .audio_metrics import AudioMetrics, MetricInputData, save_embeddings
-from .audio_prompt_adherence import AccompanimentPromptAdherence
+from .apa import AccompanimentPromptAdherence
+from .apa import AccompanimentPromptAdherence as APA
 from .embed_pipeline import EmbedderPipeline
