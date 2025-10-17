31e5e172decd30fe035bdd10bcab2562a7fcee49
Maarten Grachten <maarten@grachten.eu>
2025-10-17 16:32:28 +0200

    update workflows; set __version__;

--- src/audio_metrics/__init__.py
+++ src/audio_metrics/__init__.py
@@ -1,1 +1,8 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("audio-metrics")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 from .audio_metrics import AudioMetrics
